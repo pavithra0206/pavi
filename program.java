04d69571fa21c205cc14e26d93d021371065d26b
pavithra0206 <47376964+pavithra0206@users.noreply.github.com>
2019-02-06 14:23:50 +0530

    Update program.java

--- program.java
+++ program.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 class PosNegzero {
    public static void main(String args[]){
       int num;
-      System.out.println("Enter a number ::");
       Scanner sc = new Scanner(System.in);
       num = sc.nextInt();
 
       if (num > 0){
-         System.out.println("Given number is a positive integer");
+         System.out.println("positive");
       } else if(num < 0){
-         System.out.println("Given number is a negative integer");
+         System.out.println("negative");
       } else {
-         System.out.println("Given number is a nor negative integer");
+         System.out.println("zero");
       }
    }
  }
